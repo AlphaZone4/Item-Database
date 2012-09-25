464344116b77f6d6fe6ca83a2a1bafce21065b80
Cubehouse <cubehouse@alphazone4.com>
2012-09-25 09:42:14 +0100

    Build some support for HTML encoding/decoding

--- js/snippets.js
+++ js/snippets.js
@@ -1,2 +1,11 @@
 // on_resize minisnippet from https://github.com/louisremi/jquery-smartresize
 function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,100)};return c};
+
+// jQuery-ised HTML encoding/decoding
+function htmlEncode(value){
+  return $('<div/>').text(value).html();
+}
+
+function htmlDecode(value){
+  return $('<div/>').html(value).text();
+}
