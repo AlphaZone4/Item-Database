cad37100ae5ff066103c49708f0e2f6f80c95dd5
Cubehouse <cubehouse@alphazone4.com>
2012-10-14 23:07:12 +0100

    Add breadcrumb to frame

--- js/src/config.js
+++ js/src/config.js
@@ -8,6 +8,25 @@ define(function(){
         
         cdnBase: "http://cdn.beta.alphazone4.com",
         
+        home2Dat: {
+            1: {
+                "region": "Europe",
+                "flag"  : "flag_eu"
+            },
+            110: {
+                "region": "North America",
+                "flag"  : "flag_us"
+            },
+            383: {
+                "region": "Japan",
+                "flag"  : "flag_jp"
+            },
+            286: {
+                "region": "Asia",
+                "flag"  : "flag_hk"
+            }
+        },
+        
         // module header
         module: "configuration"
     };
