365e81d52ca14605dcc3c42d1f66cc1ffd168966
Cubehouse <cubehouse@alphazone4.com>
2012-10-27 22:56:53 +0100

    Tify up snippets.js formatting

--- js/snippets.js
+++ js/snippets.js
@@ -2,10 +2,10 @@
 function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,100)};return c};
 
 // jQuery-ised HTML encoding/decoding
-function htmlEncode(value){
+function htmlEncode(value) {
   return $('<div/>').text(value).html();
 }
 
-function htmlDecode(value){
+function htmlDecode(value) {
   return $('<div/>').html(value).text();
 }
