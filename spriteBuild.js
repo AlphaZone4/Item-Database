8434313b7fa77b6182e5c2952a49422ae91c67a1
Cubehouse <cubehouse@alphazone4.com>
2012-12-01 16:18:27 +0000

    Fix sprite builder

--- spriteBuild.js
+++ spriteBuild.js
@@ -1,13 +1,16 @@
 var Builder = require('node-spritesheet').Builder;
-var res = [];
 require("fs").readdir("images", function(err, images) {
     for(var ii=0; ii<images.length; ii++) {
         images[ ii ] = "images/"+images[ ii ];
     }
-    var b = new Builder( images, res, {
-        outputImage: __dirname+'/css/sprite.png',
-        outputCss: __dirname+'/css/sprite.css',
-        selector: '.az4im'
+    var b = new Builder({
+        outputDirectory: __dirname+'/css/',
+        outputImage: 'sprite.png',
+        outputCss: 'sprite.css',
+        selector: '.az4im',
+        images: images
     });
-    b.build();
+    b.build(function(res) {
+        // console.log(res);
+    });
 });
