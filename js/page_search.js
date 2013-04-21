43fce2357fa952b696f5b199174dc36f942f6e57
Cubehouse <cubehouse@alphazone4.com>
2013-04-21 14:21:52 +0100

    Add basic search stats to results page

--- js/page_search.js
+++ js/page_search.js
@@ -1,10 +1,12 @@
-define(["frame", "encrypt", "api", "lists", "forms", "msg"], function(frame, encrypt, api, lists, form, msg) {
+define(["frame", "encrypt", "api", "lists", "forms", "msg", "config"], function(frame, encrypt, api, lists, form, msg, _config) {
     // valid search parameters
     var valid = [
         "query"
     ];
     
     var item_list;
+    
+    var stats = $("<div>").css("clear", "both");
     
     var do_search = function(opt) {
         // load search results!
@@ -13,6 +15,9 @@ define(["frame", "encrypt", "api", "lists", "forms", "msg"], function(frame, enc
                 msg.error(data.error);
                 item_list.loadPreload([]);
             } else {
+                // show basic stats
+                stats.html("<strong>"+data.results+"</strong> results in "+((data.data_time + data.search_time) / 1000) + " seconds");
+                
                 item_list.loadPreload(data.items);
             }
         });
@@ -20,6 +25,8 @@ define(["frame", "encrypt", "api", "lists", "forms", "msg"], function(frame, enc
     
     var search_options = function(opts) {
         if (!opts) opts = {};
+        
+        var holder = $("<div>");
         
         // load search form
         var f = form(
@@ -46,7 +53,8 @@ define(["frame", "encrypt", "api", "lists", "forms", "msg"], function(frame, enc
                             value: "F",
                             name: "Female"
                         }
-                    ]
+                    ],
+                    value: (opts.gender == "" || !opts.gender) ? "" : ( opts.gender == "M" ? "M" : "F" )
                 },
                 {
                     type: "radio",
@@ -82,10 +90,55 @@ define(["frame", "encrypt", "api", "lists", "forms", "msg"], function(frame, enc
             ]
         );
         
+        // add some custom search parameters
+        var opts = $("<div>").css("width", "400px").css("float", "left");
+        
+        // add developer filter
+        var current_dev_filters = [];
+        var dev_filter_box = $("<div>");
+        var dev_choose = $("<select id='dev_filter_list'>");
+        for(var ii in _config.settings.devs) {
+            dev_choose.append("<option value='"+ii+"'>"+_config.settings.devs[ ii ]+"</option>");
+        }
+        var add_dev_filter = $("<button class='btn btn-primary'>Add</button>").click(function() {
+            var dev = $("#dev_filter_list").val();
+            
+            for(var ii=0; ii<current_dev_filters.length; ii++) {
+                if (current_dev_filters[ii] == dev) {
+                    return;
+                }
+            }
+            
+            current_dev_filters.push(dev);
+            var label = $("<span class='label label-info'>"+_config.settings.devs[dev]+"</span>");
+            label.click(function() {
+                for(var ii=0; ii<current_dev_filters.length; ii++) {
+                    if (current_dev_filters[ii] == dev) {
+                        current_dev_filters.splice(ii, 1);
+                    }
+                    
+                    $(this).remove();
+                }
+            });
+            dev_filter_box.append(label);
+        });
+        // disable for now
+        //opts.append("Filter by Developer: ").append(dev_choose).append(add_dev_filter).append(dev_filter_box);
+        
+        // called when form is submitted
         f.submit(function() {
             var opts = f.serializeObject();
             
-            var URL = encrypt.base64_encode(JSON.stringify(opts));
+            // add developer filters
+            //console.log(current_dev_filters);
+            // TODO - do properly
+            if (current_dev_filters[0]) {
+                opts.dev = current_dev_filters[0];
+            }
+            
+            // strip equals signs from URL, they're just padding anyway.
+            var URL = encrypt.base64_encode(JSON.stringify(opts)).replace(/\=/g, '');
+            
             az4db_do("pageChange", "search/"+URL);
             
             do_search(opts);
@@ -93,7 +146,11 @@ define(["frame", "encrypt", "api", "lists", "forms", "msg"], function(frame, enc
             return false;
         });
         
-        return f;
+        f.css("width", "320px").css("float", "left");
+        
+        holder.append(f).append(opts);
+        
+        return holder;
     };
     
     frame.add_page(/^search(?:$|\/([0-9a-zA-Z+\/=]+)?$)/, function(m) {
@@ -120,6 +177,9 @@ define(["frame", "encrypt", "api", "lists", "forms", "msg"], function(frame, enc
         frame.page.html("<h2>Item Database Search</h2>");
         
         frame.page.append(search_options(obj));
+        
+        stats.html("");
+        frame.page.append(stats)
         
         item_list = lists.create(null, null);
         frame.page.append(item_list.body);
