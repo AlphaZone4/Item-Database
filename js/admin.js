10daae5a9038623f10fe54b2cdd5f7c22cea18fc
Cubehouse <cubehouse@alphazone4.com>
2013-04-21 13:19:11 +0100

    Add mass item editor mode for admins

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-define(["config", "popup", "api", "msg", "items", "jquery", "jqueryui/sortable"], function(_config, popup, api, msg, items) {
+define(["config", "popup", "api", "msg", "items", "forms", "jquery", "jqueryui/sortable"], function(_config, popup, api, msg, items, forms) {
     // array of function pointers for various admin menus
     var menus = [
         admin_menu
@@ -278,6 +278,250 @@ define(["config", "popup", "api", "msg", "items", "jquery", "jqueryui/sortable"]
                             controls.html(
                                 $("<button>").addClass("btn btn-warning")
                                 .text("Stop Sorting")
+                                .click(function() {
+                                    list.reload();
+                                })
+                            );
+                            
+                            menu_div.append(controls);
+                        });
+                        
+                        return false;
+                    }
+                }
+            );
+        }
+        
+        // mass editor
+        if (_config.settings.database_edit && (list.type=="cat" || list.type=="update") && data && data.items && data.items.length ) {
+            menus.push(
+                {
+                    name: "Mass Edit",
+                    func: function() {
+                        // remember current list page setting
+                        var mem = list.page_items;
+                        list.page_items = 0;
+                        
+                        // reload the list without any pages on
+                        list.reload(function(){
+                            // grab actual list object
+                            var l = $(list.body.find(".az4list")[0]);
+                            
+                            var items2edit = [];
+                            
+                            // remove item click handlers and replace with item selector
+                            l.find("a.thumbnail").each(function() {
+                                $(this).off('click');
+                                $(this).click(function() {
+                                    var found = -1;
+                                    for(var ii=0; ii<items2edit.length; ii++) {
+                                        if (items2edit[ ii ] == $(this).parent().attr("id")) {
+                                            found = ii;
+                                        }
+                                    }
+                                    
+                                    if (found >= 0) {
+                                        $(this).removeClass("highlight");
+                                        
+                                        items2edit.splice(found, 1);
+                                    } else {
+                                        $(this).addClass("highlight");
+                                        
+                                        items2edit.push(parseInt($(this).parent().attr("id")));
+                                    }
+                                    
+                                    return false;
+                                });
+                            });
+                            
+                            // now that list is reloaded, restore item page number
+                            list.page_items = mem;
+                            
+                            controls.html("");
+                            
+                            // setup mass edit options
+                            var options = [];
+                            for(var ii in _config.settings.prices) {
+                                options.push({
+                                    name: _config.settings.prices[ ii ].name+" "+ii,
+                                    value: _config.settings.prices[ ii ].field
+                                });
+                            }
+                            options.push({
+                                name: "Furniture Slots",
+                                value: "slots"
+                            });
+                            
+                            var inputs = [
+                                {
+                                    type: "dropdown",
+                                    name: "function",
+                                    options: options,
+                                    change: function() {
+                                        // modify form if we have a special dropdown
+                                        var new_type = $(this).attr("value");
+                                        
+                                        if (dropdowns[ new_type ]) {
+                                            // we have a dropdown!
+                                            $(".massedit_text").hide();
+                                            $(".massedit_dropdown").show();
+                                            
+                                            // build new list of dropdowns
+                                            var h = "";
+                                            for(var ii=0; ii<dropdowns[ new_type ].length; ii++) {
+                                                h += "<option value='"+dropdowns[ new_type ][ii].value+"'>"+dropdowns[ new_type ][ii].name+"</option>";
+                                            }
+                                            
+                                            $(".massedit_dropdown").html(h);
+                                        } else {
+                                            // normal text entry
+                                            $(".massedit_text").show();
+                                            $(".massedit_dropdown").hide();
+                                        }
+                                    }
+                                },
+                                {
+                                    type: "text",
+                                    name: "value_text",
+                                    cssclass: "massedit_text"
+                                },
+                                {
+                                    type: "dropdown",
+                                    name: "value_dropdown",
+                                    options: [],
+                                    cssclass: "massedit_dropdown hide"
+                                },
+                                {
+                                    type: "submit",
+                                    value: "Make Changes"
+                                }
+                            ];
+                            
+                            // some editing functions can have dropdowns instead of text entry
+                            var dropdown_devs = [];
+                            for(var ii in _config.settings.devs) {
+                                dropdown_devs.push({
+                                    name: _config.settings.devs[ ii ],
+                                    value: ii
+                                });
+                            }
+                            
+                            var dropdown_types = [];
+                            for(var ii in _config.settings.item_types) {
+                                dropdown_types.push({
+                                    name: _config.settings.item_types[ ii ],
+                                    value: ii
+                                });
+                            }
+                            
+                            var dropdowns = {
+                                gender: [
+                                    {
+                                        name: "None",
+                                        value: ""
+                                    },
+                                    {
+                                        name: "Male",
+                                        value: "M"
+                                    },
+                                    {
+                                        name: "Female",
+                                        value: "F"
+                                    }
+                                ],
+                                dev: dropdown_devs,
+                                type: dropdown_types
+                            };
+                            
+                            var dropdown_names = {
+                                gender: "Gender",
+                                dev: "Developer",
+                                type: "Item Type"
+                            };
+                            
+                            // add dropdowns to inputs list
+                            for(var ii in dropdowns) {
+                                options.push({
+                                    name: dropdown_names[ ii ],
+                                    value: ii
+                                });
+                            }
+                            
+                            var form = forms(inputs, function(form_data, reset) {
+                                if (!items2edit.length) return;
+                                
+                                var data = {
+                                    ids: items2edit.join(",")
+                                };
+                                
+                                // choose which data input to use
+                                if (dropdowns[ form_data['function'] ]) {
+                                    data[ form_data['function'] ] = form_data['value_dropdown'];
+                                } else {
+                                    data[ form_data['function'] ] = form_data['value_text'];
+                                }
+                                
+                                api.post("edit/items", data, function(result) {
+                                    if (result.success) {
+                                        msg.success(result.success);
+                                    } else if (result.error) {
+                                        msg.error(result.error);
+                                    }
+                                });
+                                
+                                // allow form to be resubmitted
+                                reset();
+                            });
+                            
+                            controls.append(form);
+                            
+                            // select all button
+                            controls.append(
+                                $("<button>").addClass("btn btn-info")
+                                .text("Select All")
+                                .click(function() {
+                                    l.find("a.thumbnail").each(function() {
+                                        if (!$(this).hasClass("highlight")) {
+                                            $(this).click();
+                                        }
+                                    });
+                                    
+                                    return false;
+                                })
+                            );
+                            
+                            // select none button
+                            controls.append(
+                                $("<button>").addClass("btn btn-info")
+                                .text("Select None")
+                                .click(function() {
+                                    l.find("a.thumbnail").each(function() {
+                                        if ($(this).hasClass("highlight")) {
+                                            $(this).click();
+                                        }
+                                    });
+                                    
+                                    return false;
+                                })
+                            );
+                            
+                            // invert button
+                            controls.append(
+                                $("<button>").addClass("btn btn-info")
+                                .text("Invert Selection")
+                                .click(function() {
+                                    l.find("a.thumbnail").each(function() {
+                                        $(this).click();
+                                    });
+                                    
+                                    return false;
+                                })
+                            );
+                            
+                            // cancel button
+                            controls.append(
+                                $("<button>").addClass("btn btn-warning")
+                                .text("Stop Editing")
                                 .click(function() {
                                     list.reload();
                                 })
