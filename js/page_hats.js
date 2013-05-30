d9d5bb99d1a1cb44258c383f9ef8ca4ea0f184df
Cubehouse <cubehouse@alphazone4.com>
2013-05-30 22:49:27 +0100

    Proud of link

--- js/page_hats.js
+++ js/page_hats.js
@@ -4,7 +4,7 @@ define(["jquery", "frame", "api"], function($, frame, api) {
 		
 		var h = "<h2>AlphaZone4 Hats</h2>";
         
-        h += "<p>Share this page</p>";
+        h += "<p>Share this page - http://psho.me/hat</p>";
         h += '<p><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://psho.me/hat" data-text="[US] Free AlphaZone4 Cake Hat!" data-via="AlphaZone4" data-size="large">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\'://platform.twitter.com/widgets.js\';fjs.parentNode.insertBefore(js,fjs);}}(document, \'script\', \'twitter-wjs\');</script></p>';
         h += '<div id="fb-root"></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=170199966382591";fjs.parentNode.insertBefore(js, fjs);}(document, "script", "facebook-jssdk"));</script>';
         h += '<div class="fb-like" data-href="http://alphazone4.com/store/expo2013/hats/" data-send="true" data-width="450" data-show-faces="false" data-font="verdana" data-action="recommend"></div>';
